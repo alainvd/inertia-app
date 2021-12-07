97d3a5d1bc5569aee050976818b821be6eccbec9
Alain Van Driessche <alainvd@gmail.com>
2021-12-07 16:11:19 +0100

    First Commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1> Hello superb {{ name }} with docker ! </h1>
+
+    <div>Frameworks</div>
+    <ol>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ol>
+
+</template>
+
+
+<script>
+export default {
+
+    props: {
+        name: String,
+        frameworks: Array
+    }
+};
+
+
+</script>
